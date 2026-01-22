75a854bd05b0af6a285666499881ee0ad455974e
Tamer Fahmy <tamfahmy@meta.com>
2026-01-22 00:11:20 -0500

    Update robots.txt with canonical domain documentation
    Clarify that https://tamerfahmy.net/ is the canonical domain
    and all other variants (http://, www.) redirect automatically

--- scripts/check-ssl-cert.sh
+++ scripts/check-ssl-cert.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # SSL Certificate Monitor for tamerfahmy.net
 # This script checks the SSL certificate expiration date and sends a reminder
-# 
+#
 # Usage: Run monthly with cron:
 # 0 9 1 * * /path/to/check-ssl-cert.sh
 #
@@ -41,7 +41,7 @@ if [ "$DAYS_REMAINING" -lt "$WARN_DAYS" ]; then
     echo "  - If not renewed, check GitHub Pages settings"
     echo "  - Repository: https://github.com/Tamer-T-Fahmy/TamerFahmy.github.io"
     echo ""
-    
+
     # Optional: Send email notification (uncomment if mail is configured)
     # echo "SSL certificate for $DOMAIN expires in $DAYS_REMAINING days!" | mail -s "SSL Certificate Warning: $DOMAIN" $EMAIL
 else
